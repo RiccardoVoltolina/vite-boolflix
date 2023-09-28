989b07dccf0ad2d2ca74dac7708661a707ab04d1
Riccardo <riky.volto@live.com>
2023-09-28 17:48:11 +0200

    create search component

--- src/App.vue
+++ src/App.vue
@@ -1,54 +1,20 @@
 <script>
   import AppSearch from './components/AppSearch.vue';
-/*   import  {store}  from './store.js';
- */  import axios from 'axios'
+  import axios from 'axios'
   export default {
+    name: 'App',
     data() {
       return {
-        AppSearch,
       }
     },
-  /*   created() {
-      store.fetchData();
-    }, */
-    methods: {
-
-      //al click stampo i risultati filtrati
-
-     /*  searchFilms() {
-
-        //cambio il mio url dinamicamente
-
-        this.store.base_url = this.store.base_url + `&query=${store.movies}`
-        console.log(this.store.base_url);
-        this.store.fetchData()
-        
-       } */
-      
+    components: {
+      AppSearch
     },
   }
 </script>
 
 <template>
   <AppSearch/>
-<!--   <header>
-    <input  v-model="store.movies" type="text" name="search" id="">
-    <button @click="searchFilms()">CERCA IL TUO FILM</button>
-    <div class="container">
-      <ul v-for="(film, i) in this.store.selectedFilm">
-        <li class="d-flex flex-column">
-          <div>TITOLO: {{ this.store.selectedFilm[i].title }}</div>
-          <div>TITOLO DEL FILM: {{ this.store.selectedFilm[i].original_title}}</div>
-          <div>LINGUA: {{ this.store.selectedFilm[i].original_language }}</div>
-          <div>VOTO: {{ this.store.selectedFilm[i].vote_average }}</div>
-        </li>
-
-      </ul>
-    </div>
-  </header>
- -->
-
-
 </template>
 
 <style lang="scss">
