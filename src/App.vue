add4146e793bca3721b8d0ae179005c0329d5bd7
Riccardo <riky.volto@live.com>
2023-09-28 15:11:09 +0200

    add filter link function

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,26 @@
 <script>
-  import { store } from './store.js';
+  import  {store}  from './store.js';
+  import axios from 'axios'
   export default {
-    name:'App',
     data() {
       return {
         store,
       }
-    }
+    },
+    methods: {
+      searchFilms() {
+        this.store.base_url = this.store.base_url + `&query=${store.movies}`
+        console.log(this.store.base_url);
+       }
+      
+    },
   }
 </script>
 
 <template>
+  <header>
+    <input  v-model="store.movies" type="text" @keyup="searchFilms()" name="search" id="">
+  </header>
 
 
 
--- src/store.js
+++ src/store.js
@@ -1,6 +1,11 @@
-import {reactive} from 'vue';
-/* import axios from "axios";
- */
-export const store = reactive ({
+import { reactive } from 'vue'
+import axios from 'axios'
+export const store = reactive({
+  base_url: 'https://api.themoviedb.org/3/search/movie?api_key=6e87e9e8ed25b27ad31bd55380aa45ed',
+  selectedFilm: '',
+  movies: [],
+ 
 
+
+ 
 })
