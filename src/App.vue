5d95b163ae30c761e08089f56d0f05dd8eb17342
Riccardo <riky.volto@live.com>
2023-10-02 14:42:30 +0200

    add AppHeader component

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,8 @@
 <script>
   import { store } from './store.js';
 
-  import AppSearch from './components/AppSearch.vue';
-  import AppHeader from './components/AppHeader.vue';
-  import axios from 'axios'
+/*   import AppSearch from './components/AppSearch.vue';
+ */  import AppHeader from './components/AppHeader.vue';
   export default {
     name: 'App',
     data() {
@@ -66,73 +65,17 @@
             return starValue
         },
 
-        //al click stampo i risultati filtrati
-
-        searchFilms() {
-
-            //cambio il mio url dinamicamente
-            this.store.base_url = `${this.store.base_urlN}api_key=e99307154c6dfb0b4750f6603256716d&query=${store.movies}`
-            this.store.fetchData()
-        }
-
-
     },
     components: {
-      /* AppSearch,
-      AppHeader */
+      /* AppSearch,*/
+      AppHeader 
     },
   }
 </script>
 
 <template>
   <!-- <AppSearch/> -->
-  <header>
-        <nav>
-            <div class="container">
-                <div class="row">
-                    <div class="d-flex align-items-center justify-content-between">
-                        <div class="left_side_header d-flex align-items-center">
-                            <img src="./assets/img/logoNetflix.png" alt="">
-                            <div class="w_470px d-sm-none d-md-block">
-                                <input v-model="store.movies" type="text" name="search" id="">
-                                <button @click="searchFilms()">CERCA</button>
-                            </div>
-                            <a class="d-sm-none d-md-none d-lg-block" href="#">Home</a>
-                            <a class="d-sm-none d-md-none d-lg-block" href="#">Serie TV</a>
-                            <a class="d-sm-none d-md-none d-lg-block" href="#">Nuovi Popolari</a>
-                            <a class="d-sm-none d-md-none d-lg-block" href="#">La mia lista</a>
-                        </div>
-                        <div class="right_side_header d-flex align-items-center">
-                            <a class="d-sm-none d-md-none d-lg-block" href="#"><i
-                                    class="fa-solid fa-magnifying-glass"></i></a>
-                            <a class="d-sm-none d-md-none d-lg-block" href="#">Bambini</a>
-                            <a class="d-sm-none d-md-none d-lg-block" href="#">DVD</a>
-                            <a class="d-sm-none d-md-none d-lg-block" href="#"><i class="fa-solid fa-bell"></i></a>
-                            <img src="./assets/img/Netflix-avatar.png" alt="">
-                            <div class="dropdown">
-                                <button class="btn dropdown-toggle border-0 text-white" type="button"
-                                    data-bs-toggle="dropdown" aria-expanded="false">
-
-                                </button>
-                                <ul class="dropdown-menu" style="">
-                                    <li class="d-flex align-items-center bg-danger-subtle ps-3"><img
-                                            src="./assets/img/Netflix-avatar.png" alt=""> <a class="dropdown-item text-black"
-                                            href="#">Riky</a></li>
-                                    <li class="d-flex align-items-center ps-3"><img src="./assets/img/Netflix-avatar.png" alt="">
-                                        <a class="dropdown-item text-black" href="#">Nimesh</a>
-                                    </li>
-                                    <li class="d-flex align-items-center bg-dark-subtle ps-3"><img
-                                            src="./assets/img/Netflix-avatar.png" alt=""> <a class="dropdown-item text-black"
-                                            href="#">Fabio</a></li>
-                                </ul>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </nav>
-    </header>
-
+<AppHeader/>
     <main>
         <div class="container">
             <div class="d-flex justify-content-between pt-4">
@@ -206,55 +149,15 @@
 
 <style lang="scss">
  @use './assets/app';
-main {
-    height: 100%;
-}
 
-.language_flag {
-    width: 10px;
-}
-
-.star_svg {
-    width: 20px;
-}
-
-body a:hover {
-    color: red;
-}
-
-header {
-    height: 80px;
-    background-color: black;
-}
-
-.left_side_header img {
-    height: 80px;
-}
-
-.left_side_header a {
-    text-decoration: none;
-    color: white;
-    margin-left: 0.5rem;
-}
-
-.right_side_header img {
-    height: 30px;
-}
-
-nav .w_470px {
-    width: 470px;
-}
-
-.right_side_header a {
-    margin-right: 0.5rem;
-    text-decoration: none;
-    color: white;
-}
 
 /* start main */
 
+
 main {
     background-color: black;
+    height: 100%;
+
 }
 
 /* insieme per il nostro pianeta */
@@ -289,6 +192,9 @@ main {
 }
 
 #star_svg {
+    width: 10px;
+}
+.language_flag {
     width: 10px;
 }
 
